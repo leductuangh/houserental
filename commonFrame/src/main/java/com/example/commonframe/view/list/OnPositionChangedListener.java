f9f1691df20c155060fd03c493a6dde0567db0a8
Tyrael <leductuan@Tyraels-MacBook-Pro.local>
2015-04-29 00:23:45 +0700

    delete all

--- commonFrame/src/main/java/com/example/commonframe/view/list/OnPositionChangedListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.commonframe.view.list;
-
-import android.view.View;
-import android.widget.ListView;
-
-public interface OnPositionChangedListener {
-	public void onPositionChanged(ListView listView, int position,
-			View scrollBarPanel);
-}
--- commonFrame/src/main/java/com/example/commonframe/view/list/PinableSectionInterface.java
+++
@@ -1,6 +0,0 @@
-package com.example.commonframe.view.list;
-
-public interface PinableSectionInterface {
-	/** This method shall return 'true' if views of given type has to be pinned. */
-	boolean isItemViewTypePinned(int viewType);
-}
