99774c5f69e1f67bd77d57eed477192194a95391
leductuan <leductuan.mail@gmail.com>
2015-05-05 22:42:38 +0700

    Merge from eclipse

--- commonFrame/src/main/java/com/example/commonframe/base/BaseFragment.java
+++ commonFrame/src/main/java/com/example/commonframe/base/BaseFragment.java
@@ -14,6 +14,7 @@ import com.example.commonframe.dialog.DecisionDialog.DecisionDialogListener;
 import com.example.commonframe.dialog.Option;
 import com.example.commonframe.dialog.OptionsDialog.OptionsDialogListener;
 import com.example.commonframe.model.base.Param;
+import com.example.commonframe.util.CentralApplication;
 import com.example.commonframe.util.Constant;
 import com.example.commonframe.util.Constant.RequestTarget;
 import com.example.commonframe.util.SingleClick;
@@ -23,11 +24,16 @@ import com.example.commonframe.util.Utils;
 
 public abstract class BaseFragment extends Fragment implements BaseInterface,
 		SingleClickListener {
-	
+
 	/**
 	 * The single click to handle click action for this screen
 	 */
 	private SingleClick singleClick = null;
+
+	/**
+	 * Local active activity, in case the getActivity return null;
+	 */
+	private BaseFragmentActivity activeActivity;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -51,9 +57,19 @@ public abstract class BaseFragment extends Fragment implements BaseInterface,
 	}
 
 	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		activeActivity = (BaseFragmentActivity) activity;
+	}
+
+	@Override
 	public void startActivityForResult(Intent intent, int requestCode) {
 		if (getActivity() != null)
 			getActivity().startActivityForResult(intent, requestCode);
+		else if (getActiveActivity() != null)
+			getActiveActivity().startActivityForResult(intent, requestCode);
+		else
+			activeActivity.startActivityForResult(intent, requestCode);
 	}
 
 	@Override
@@ -67,79 +83,171 @@ public abstract class BaseFragment extends Fragment implements BaseInterface,
 
 	@Override
 	public SingleTouch getSingleTouch() {
-		return ((BaseFragmentActivity) getActivity()).getSingleTouch();
+		if (getActivity() != null
+				&& getActivity() instanceof BaseFragmentActivity)
+			return ((BaseFragmentActivity) getActivity()).getSingleTouch();
+		else if (getActiveActivity() != null
+				&& getActiveActivity() instanceof BaseFragmentActivity)
+			return ((BaseFragmentActivity) getActiveActivity())
+					.getSingleTouch();
+		else
+			return activeActivity.getSingleTouch();
 	}
 
 	@Override
 	public void showAlertDialog(Context context, int id, String title,
 			String message, int icon_id, AlertDialogListener listener) {
-		((BaseFragmentActivity) getActivity()).showAlertDialog(context, id,
-				title, message, icon_id, listener);
+		if (getActivity() != null
+				&& getActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActivity()).showAlertDialog(
+					getActivity(), id, title, message, icon_id, listener);
+		else if (getActiveActivity() != null
+				&& getActiveActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActiveActivity()).showAlertDialog(
+					getActiveActivity(), id, title, message, icon_id, listener);
+		else
+			activeActivity.showAlertDialog(activeActivity, id, title, message,
+					icon_id, listener);
 	}
 
 	@Override
 	public void showLoadingDialog(Context context) {
-		((BaseFragmentActivity) getActivity()).showLoadingDialog(context);
+		if (getActivity() != null
+				&& getActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActivity())
+					.showLoadingDialog(getActivity());
+		else if (getActiveActivity() != null
+				&& getActiveActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActiveActivity())
+					.showLoadingDialog(getActiveActivity());
+		else
+			activeActivity.showLoadingDialog(activeActivity);
 	}
 
 	@Override
 	public void showDecisionDialog(Context context, int id, String title,
 			String message, String yes, String no,
 			DecisionDialogListener listener) {
-		((BaseFragmentActivity) getActivity()).showDecisionDialog(context, id,
-				title, message, yes, no, listener);
+		if (getActivity() != null
+				&& getActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActivity()).showDecisionDialog(
+					getActivity(), id, title, message, yes, no, listener);
+		else if (getActiveActivity() != null
+				&& getActiveActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActiveActivity()).showDecisionDialog(
+					getActiveActivity(), id, title, message, yes, no, listener);
+		else
+			activeActivity.showDecisionDialog(activeActivity, id, title,
+					message, yes, no, listener);
 	}
 
 	@Override
 	public void showOptionsDialog(Context context, int id, String title,
 			String message, int icon, Option[] options,
 			OptionsDialogListener listener) {
-		((BaseFragmentActivity) getActivity()).showOptionsDialog(context, id,
-				title, message, icon, options, listener);
+		if (getActivity() != null
+				&& getActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActivity()).showOptionsDialog(
+					getActivity(), id, title, message, icon, options, listener);
+		else if (getActiveActivity() != null
+				&& getActiveActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActiveActivity()).showOptionsDialog(
+					getActiveActivity(), id, title, message, icon, options,
+					listener);
+		else
+			activeActivity.showOptionsDialog(activeActivity, id, title,
+					message, icon, options, listener);
 	}
 
 	@Override
 	public void closeLoadingDialog() {
-		((BaseFragmentActivity) getActivity()).closeLoadingDialog();
+		if (getActivity() != null
+				&& getActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActivity()).closeLoadingDialog();
+		else if (getActiveActivity() != null
+				&& getActiveActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActiveActivity()).closeLoadingDialog();
+		else
+			activeActivity.closeLoadingDialog();
 	}
 
 	@Override
 	public void cancelBackgroundRequest(String tag) {
-		((BaseFragmentActivity) getActivity()).cancelBackgroundRequest(tag);
+		if (getActivity() != null
+				&& getActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActivity()).cancelBackgroundRequest(tag);
+		else if (getActiveActivity() != null
+				&& getActiveActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActiveActivity())
+					.cancelBackgroundRequest(tag);
+		else
+			activeActivity.cancelBackgroundRequest(tag);
 	}
 
 	@Override
 	public void cancelWebServiceRequest(String tag) {
-		((BaseFragmentActivity) getActivity()).cancelWebServiceRequest(tag);
+		if (getActivity() != null
+				&& getActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActivity()).cancelWebServiceRequest(tag);
+		else if (getActiveActivity() != null
+				&& getActiveActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActiveActivity())
+					.cancelWebServiceRequest(tag);
+		else
+			activeActivity.cancelWebServiceRequest(tag);
 	}
 
 	@Override
 	public void makeBackgroundRequest(String tag, RequestTarget target,
 			String[] extras, Param content) {
-		((BaseFragmentActivity) getActivity()).makeBackgroundRequest(tag,
-				target, extras, content);
+		if (getActivity() != null
+				&& getActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActivity()).makeBackgroundRequest(tag,
+					target, extras, content);
+		else if (getActiveActivity() != null
+				&& getActiveActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActiveActivity()).makeBackgroundRequest(
+					tag, target, extras, content);
+		else
+			activeActivity.makeBackgroundRequest(tag, target, extras, content);
 	}
 
 	@Override
 	public void makeRequest(String tag, RequestTarget target, String[] extras,
 			Param content, WebServiceResultHandler handler) {
-		((BaseFragmentActivity) getActivity()).makeRequest(tag, target, extras,
-				content, handler);
+		if (getActivity() != null
+				&& getActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActivity()).makeRequest(tag, target,
+					extras, content, handler);
+		else if (getActiveActivity() != null
+				&& getActiveActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActiveActivity()).makeRequest(tag,
+					target, extras, content, handler);
+		else
+			activeActivity.makeRequest(tag, target, extras, content, handler);
 	}
 
 	@Override
 	public String getResouceString(int id) {
-		return ((BaseFragmentActivity) getActivity()).getResouceString(id);
+		if (getActivity() != null
+				&& getActivity() instanceof BaseFragmentActivity)
+			return ((BaseFragmentActivity) getActivity()).getResouceString(id);
+		else if (getActiveActivity() != null
+				&& getActiveActivity() instanceof BaseFragmentActivity)
+			return ((BaseFragmentActivity) getActiveActivity())
+					.getResouceString(id);
+		else
+			return activeActivity.getResouceString(id);
 	}
 
 	@Override
 	public Activity getActiveActivity() {
-		return ((BaseFragmentActivity) getActivity()).getActiveActivity();
+		return CentralApplication.getActiveActivity();
 	}
 
 	@Override
 	public Context getCentralContext() {
-		return ((BaseFragmentActivity) getActivity()).getCentralContext();
+		return CentralApplication.getContext();
 	}
 
 	@Override
@@ -160,45 +268,64 @@ public abstract class BaseFragment extends Fragment implements BaseInterface,
 	}
 
 	@Override
-	public void onPause() {
-		super.onPause();
-	}
-
-	@Override
-	public void onStop() {
-		super.onStop();
-	}
-
-	@Override
 	public void onDetach() {
 		onFreeObject();
 		Utils.nullViewDrawablesRecursive(getView());
 		Utils.unbindDrawables(getView());
 		super.onDetach();
+		activeActivity = null;
 	}
 
 	protected View findViewById(int id) {
-		View view = getView().findViewById(id);
-		if (view != null && !BaseProperties.isExceptionalView(view)) {
-			view.setOnClickListener(getSingleClick());
-			view.setOnTouchListener(getSingleTouch());
+		if(getView() != null) {
+			View view = getView().findViewById(id);
+			if (view != null && !BaseProperties.isExceptionalView(view)) {
+				view.setOnClickListener(getSingleClick());
+				view.setOnTouchListener(getSingleTouch());
+			}
+			return view;
 		}
-		return view;
+		return null;
 	}
 
 	protected void finish() {
-		((BaseFragmentActivity) getActivity()).onBackPressed();
+		if (getActivity() != null
+				&& getActivity() instanceof BaseFragmentActivity)
+			getActivity().onBackPressed();
+		else if (getActiveActivity() != null
+				&& getActiveActivity() instanceof BaseFragmentActivity)
+			getActiveActivity().onBackPressed();
+		else
+			activeActivity.onBackPressed();
+
 	}
 
 	protected void addFragment(int containerId, BaseFragment fragment,
 			String tag) {
-		((BaseFragmentActivity) getActivity()).addFragment(containerId,
-				fragment, tag);
+		if (getActivity() != null
+				&& getActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActivity()).addFragment(containerId,
+					fragment, tag);
+		else if (getActiveActivity() != null
+				&& getActiveActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActiveActivity()).addFragment(
+					containerId, fragment, tag);
+		else
+			activeActivity.addFragment(containerId, fragment, tag);
 	}
 
 	protected void replaceFragment(int containerId, BaseFragment fragment,
 			String tag, boolean clearStack) {
-		((BaseFragmentActivity) getActivity()).replaceFragment(containerId,
-				fragment, tag, clearStack);
+		if (getActivity() != null
+				&& getActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActivity()).replaceFragment(containerId,
+					fragment, tag, clearStack);
+		else if (getActiveActivity() != null
+				&& getActiveActivity() instanceof BaseFragmentActivity)
+			((BaseFragmentActivity) getActiveActivity()).replaceFragment(
+					containerId, fragment, tag, clearStack);
+		else
+			activeActivity.replaceFragment(containerId, fragment, tag,
+					clearStack);
 	}
 }
