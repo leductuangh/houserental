6c0f3baa3b4d7b26241bda98c777640de75b159c
leductuan <leductuan.mail@gmail.com>
2015-08-23 12:36:21 +0700

    Merge from eclipse

--- commonFrame/src/main/java/com/example/commonframe/core/connection/Requester.java
+++ commonFrame/src/main/java/com/example/commonframe/core/connection/Requester.java
@@ -3,8 +3,8 @@ package com.example.commonframe.core.connection;
 import android.annotation.SuppressLint;
 
 import com.example.commonframe.core.base.BaseProperties;
-import com.example.commonframe.core.connection.WebServiceRequester.WebServiceResultHandler;
 import com.example.commonframe.core.base.Param;
+import com.example.commonframe.core.connection.WebServiceRequester.WebServiceResultHandler;
 import com.example.commonframe.core.connection.queue.WebserviceElement;
 import com.example.commonframe.core.connection.queue.WebserviceElement.Type;
 import com.example.commonframe.core.connection.request.BackgroundServiceRequest;
