b5d0feaa874122af13276814c7562375940bc749
Le Duc Tuan <tuan.le@sutrixmedia.com>
2015-08-24 10:50:46 +0700

    Update request queue dao and git ignore, ignore local.properties

--- commonFrame/src/main/java/com/example/commonframe/data/RequestQueueDAO.java
+++ commonFrame/src/main/java/com/example/commonframe/data/RequestQueueDAO.java
@@ -1,7 +1,5 @@
 package com.example.commonframe.data;
 
-import java.util.HashMap;
-
 import com.activeandroid.Model;
 import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
@@ -10,114 +8,117 @@ import com.example.commonframe.util.Constant.RequestMethod;
 import com.example.commonframe.util.Constant.RequestTarget;
 import com.example.commonframe.util.Constant.RequestType;
 
+import java.util.HashMap;
+
 @Table(name = "RequestQueue")
 public class RequestQueueDAO extends Model {
 
-	@Column(name = "header")
-	private HashMap<String, String> headers;
+    @Column(name = "header")
+    private HashMap<String, String> headers;
 
-	@Column(name = "body")
-	private byte[] body;
+    @Column(name = "body")
+    private byte[] body;
 
-	@Column(name = "tag")
-	private String tag;
+    @Column(name = "tag")
+    private String tag;
 
-	@Column(name = "protocol")
-	private RequestType protocol;
+    @Column(name = "protocol")
+    private RequestType protocol;
 
-	@Column(name = "method")
-	private RequestMethod method;
+    @Column(name = "method")
+    private RequestMethod method;
 
-	@Column(name = "address")
-	private String address;
+    @Column(name = "address")
+    private String address;
 
-	@Column(name = "target")
-	private RequestTarget target;
+    @Column(name = "target")
+    private RequestTarget target;
 
-	@Column(name = "type")
-	private Type type;
+    @Column(name = "type")
+    private Type type;
 
-	@Column(name = "extra")
-	private String[] extra;
+    @Column(name = "extra")
+    private String[] extra;
 
-	public RequestQueueDAO() {
-		super();
-	}
+    public RequestQueueDAO() {
+        super();
+    }
 
-	public RequestQueueDAO(String tag, RequestType protocol,
-			RequestMethod method, String address, RequestTarget target,
-			String[] extra, HashMap<String, String> headers, byte[] body) {
-		super();
-		this.tag = tag;
-		this.protocol = protocol;
-		this.method = method;
-		this.address = address;
-		this.target = target;
-		this.extra = extra;
-		this.headers = headers;
-		this.body = body;
-	}
+    public RequestQueueDAO(String tag, RequestType protocol,
+                           RequestMethod method, String address, RequestTarget target, Type type,
+                           String[] extra, HashMap<String, String> headers, byte[] body) {
+        super();
+        this.tag = tag;
+        this.protocol = protocol;
+        this.method = method;
+        this.address = address;
+        this.target = target;
+        this.extra = extra;
+        this.headers = headers;
+        this.body = body;
+        this.type = type;
+    }
 
-	/**
-	 * @return the headers
-	 */
-	public HashMap<String, String> getHeaders() {
-		return headers;
-	}
+    /**
+     * @return the headers
+     */
+    public HashMap<String, String> getHeaders() {
+        return headers;
+    }
 
-	/**
-	 * @return the body
-	 */
-	public byte[] getBody() {
-		return body;
-	}
+    /**
+     * @return the body
+     */
+    public byte[] getBody() {
+        return body;
+    }
 
-	/**
-	 * @return the tag
-	 */
-	public String getTag() {
-		return tag;
-	}
+    /**
+     * @return the tag
+     */
+    public String getTag() {
+        return tag;
+    }
 
-	/**
-	 * @return the protocol
-	 */
-	public RequestType getProtocol() {
-		return protocol;
-	}
+    /**
+     * @return the protocol
+     */
+    public RequestType getProtocol() {
+        return protocol;
+    }
 
-	/**
-	 * @return the method
-	 */
-	public RequestMethod getMethod() {
-		return method;
-	}
+    /**
+     * @return the method
+     */
+    public RequestMethod getMethod() {
+        return method;
+    }
 
-	/**
-	 * @return the address
-	 */
-	public String getAddress() {
-		return address;
-	}
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
 
-	/**
-	 * @return the target
-	 */
-	public RequestTarget getTarget() {
-		return target;
-	}
+    /**
+     * @return the target
+     */
+    public RequestTarget getTarget() {
+        return target;
+    }
 
-	/**
-	 * @return the type
-	 */
-	public Type getType() {
-		return type;
-	}
+    /**
+     * @return the type
+     */
+    public Type getType() {
+        return type;
+    }
 
-	/**
-	 * @return the extra
-	 */
-	public String[] getExtra() {
-		return extra;
-	}
+    /**
+     * @return the extra
+     */
+    public String[] getExtra() {
+        return extra;
+    }
 }
