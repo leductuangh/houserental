c8ff94d5bc243c4f426e7cfcbec80de96534c739
leductuan <leductuan.mail@gmail.com>
2015-08-30 11:56:29 +0700

    Update google volley

--- googlevolleynetwork_lib/src/main/java/com/android/volley/RequestQueue.java
+++ googlevolleynetwork_lib/src/main/java/com/android/volley/RequestQueue.java
@@ -39,15 +39,10 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class RequestQueue {
 
-    /** Callback interface for completed requests. */
-    public interface RequestFinishedListener<T> {
-        /** Called when a request has finished processing. */
-        void onRequestFinished(Request<T> request);
-    }
-
-    /** Used for generating monotonically-increasing sequence numbers for requests. */
-    private AtomicInteger mSequenceGenerator = new AtomicInteger();
-
+    /**
+     * Number of network request dispatcher threads to start.
+     */
+    private static final int DEFAULT_NETWORK_THREAD_POOL_SIZE = 4;
     /**
      * Staging area for requests that already have a duplicate request in flight.
      *
@@ -60,40 +55,32 @@ public class RequestQueue {
      */
     private final Map<String, Queue<Request<?>>> mWaitingRequests =
             new HashMap<String, Queue<Request<?>>>();
-
     /**
      * The set of all requests currently being processed by this RequestQueue. A Request
      * will be in this set if it is waiting in any queue or currently being processed by
      * any dispatcher.
      */
     private final Set<Request<?>> mCurrentRequests = new HashSet<Request<?>>();
-
     /** The cache triage queue. */
     private final PriorityBlockingQueue<Request<?>> mCacheQueue =
         new PriorityBlockingQueue<Request<?>>();
-
     /** The queue of requests that are actually going out to the network. */
     private final PriorityBlockingQueue<Request<?>> mNetworkQueue =
         new PriorityBlockingQueue<Request<?>>();
-
-    /** Number of network request dispatcher threads to start. */
-    private static final int DEFAULT_NETWORK_THREAD_POOL_SIZE = 4;
-
     /** Cache interface for retrieving and storing responses. */
     private final Cache mCache;
-
     /** Network interface for performing requests. */
     private final Network mNetwork;
-
     /** Response delivery mechanism. */
     private final ResponseDelivery mDelivery;
-
+    /**
+     * Used for generating monotonically-increasing sequence numbers for requests.
+     */
+    private AtomicInteger mSequenceGenerator = new AtomicInteger();
     /** The network dispatchers. */
     private NetworkDispatcher[] mDispatchers;
-
     /** The cache dispatcher. */
     private CacheDispatcher mCacheDispatcher;
-
     private List<RequestFinishedListener> mFinishedListeners =
             new ArrayList<RequestFinishedListener>();
 
@@ -179,14 +166,6 @@ public class RequestQueue {
      */
     public Cache getCache() {
         return mCache;
-    }
-
-    /**
-     * A simple predicate or filter interface for Requests, for use by
-     * {@link RequestQueue#cancelAll(RequestFilter)}.
-     */
-    public interface RequestFilter {
-        boolean apply(Request<?> request);
     }
 
     /**
@@ -313,5 +292,23 @@ public class RequestQueue {
       synchronized (mFinishedListeners) {
         mFinishedListeners.remove(listener);
       }
+    }
+
+    /**
+     * Callback interface for completed requests.
+     */
+    public interface RequestFinishedListener<T> {
+        /**
+         * Called when a request has finished processing.
+         */
+        void onRequestFinished(Request<T> request);
+    }
+
+    /**
+     * A simple predicate or filter interface for Requests, for use by
+     * {@link RequestQueue#cancelAll(RequestFilter)}.
+     */
+    public interface RequestFilter {
+        boolean apply(Request<?> request);
     }
 }
