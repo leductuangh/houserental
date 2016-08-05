49e2e5fa092f635e3c6ca75d9a9c127b2a3299ba
Tyrael <leductuan.mail@gmail.com>
2016-08-05 17:15:31 +0700

    Style Setting Screen

---
+++ app/src/main/java/com/example/houserental/function/view/HouseRentalEditText.java
@@ -0,0 +1,29 @@
+package com.example.houserental.function.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+/**
+ * Created by Tyrael on 8/5/16.
+ */
+public class HouseRentalEditText extends EditText {
+    public HouseRentalEditText(Context context) {
+        super(context);
+        init();
+    }
+
+    public HouseRentalEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public HouseRentalEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+
+    }
+}
